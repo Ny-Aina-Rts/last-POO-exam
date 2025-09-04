affc94097bbf719238908df3e11abbfbd8d62e41
Ny_Aina_Rts <ny.aiina.rts@gmail.com>
2025-09-04 12:01:46 +0300

    Refactor: add params in Class: Group

--- src/main/java/Group.java
+++ src/main/java/Group.java
@@ -1,2 +1,15 @@
 public class Group {
+    private int id;
+    private String name;
+
+    public Group(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
 }
